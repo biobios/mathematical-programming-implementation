8732fad572c9646e42633fa2c6eb5c635b37d9e6
BioBIOS <biobios1024@gmail.com>
2025-07-20 14:59:05 +0900

    feat: 世代交代モデルの実装

---
+++ src/fast_eax/generational_model.hpp
@@ -0,0 +1,92 @@
+#pragma once
+
+#include <vector>
+#include <random>
+
+#include "utils.hpp"
+
+namespace eax {
+    
+    template <size_t N_cross>
+    struct GenerationalModelImpl
+    {
+        /**
+        * @brief 世代交代モデルを実装する関数オブジェクト
+        * @param population 初期集団
+        * @param end_condition 終了条件を満たすかどうかを判定する関数(オブジェクト)
+        * @param fitness_func 適応度を計算する関数(オブジェクト)
+        * @param cross_over 交叉を行う関数(オブジェクト)
+        * @param env 環境情報
+        * @param rng 乱数生成器
+        * @param logging ロギング関数(オブジェクト) (デフォルトは何もしない関数)
+        * @return 最終的な集団
+        */
+        template <typename Individual, typename EndCondition, typename FitnessFunc, typename Environment, typename CrossOverFunc, std::uniform_random_bit_generator RandomGen, typename LoggingFunc = mpi::NOP_Function>
+            requires(requires(std::vector<Individual> population, EndCondition end_condition, FitnessFunc fitness_func, CrossOverFunc cross_over, Environment env, RandomGen rng, size_t generation, LoggingFunc logging) {
+                { end_condition(population, env, generation) } -> std::convertible_to<bool>;
+                { fitness_func(population[0], population[1], env) } -> std::convertible_to<double>;
+                { cross_over(population[0], population[1], 1, env, rng) } -> std::convertible_to<std::vector<Individual>>;
+                { logging(population, env, generation) } -> std::convertible_to<void>;
+            })
+        constexpr std::vector<Individual> operator()(std::vector<Individual> population, EndCondition end_condition, FitnessFunc fitness_func, CrossOverFunc cross_over, Environment env, RandomGen rng, LoggingFunc&& logging = {}) const
+        {
+            auto calc_all_fitness = [&fitness_func](const std::vector<Individual>& pop, const Individual parent, Environment& env) {
+                std::vector<double> fitness_values(pop.size());
+                for (size_t i = 0; i < pop.size(); ++i) {
+                    fitness_values[i] = fitness_func(pop[i], parent, env);
+                }
+                return fitness_values;
+            };
+            
+            size_t generation = 0;
+
+            size_t population_size = population.size();
+            while (!end_condition(population, env, generation)) {
+                // ロギング
+                logging(population, env, generation);
+                
+                std::vector<size_t> indices(population_size);
+                std::iota(indices.begin(), indices.end(), 0);
+                std::shuffle(indices.begin(), indices.end(), rng);
+
+                population.push_back(population[indices[0]]); // 最初の個体を追加しておく
+                indices.push_back(population.size() - 1);
+                for (size_t i = 0; i < population_size; ++i) {
+                    size_t parent_A_index = indices[i];
+                    size_t parent_B_index = indices[(i + 1) % population_size];
+                    Individual& parent_A = population[parent_A_index];
+                    Individual& parent_B = population[parent_B_index];
+                    std::vector<Individual> children = cross_over(parent_A, parent_B, N_cross, env, rng);
+                    std::vector<double> children_fitness = calc_all_fitness(children, parent_A, env);
+                    
+                    // 子供の中で最良の個体を選択
+                    size_t best_index = 0;
+                    double best_fitness = children_fitness[0];
+                    for (size_t j = 1; j < children_fitness.size(); ++j) {
+                        if (children_fitness[j] > best_fitness) {
+                            best_fitness = children_fitness[j];
+                            best_index = j;
+                        }
+                    }
+                    
+                    if (best_fitness != 0.0) {
+                        // 子供の適応度が0でない場合、親Aを子供に置き換える
+                        parent_A = std::move(children[best_index]);
+                    } else {
+                        // 子供の適応度が0の場合、親Aをそのままにする
+                    }
+                }
+                
+                // 最後の個体を削除
+                population.pop_back();
+                
+                generation++;
+            }
+            
+            return population;
+        }
+    };
+    
+    template <size_t N_cross>
+    constexpr GenerationalModelImpl<N_cross> GenerationalModel;
+}
