045b3871318c9b749e4c2a9a8086e271b2784304
BioBIOS <biobios1024@gmail.com>
2025-07-31 14:13:39 +0900

    feat: ABサイクルを見つける処理を他のプロジェクトでも使えるように、fast_eaxから切り出した

---
+++ src/libeaxlib/ab_cycle_finder.hpp
@@ -0,0 +1,272 @@
+#pragma once
+
+#include <random>
+
+#include "object_pool.hpp"
+#include "limited_range_integer_set.hpp"
+
+namespace eax {
+    
+inline mpi::ObjectPool<std::vector<size_t>>::pooled_unique_ptr create_AB_cycle(std::vector<size_t>& finding_path,
+                        size_t end_index,
+                        mpi::LimitedRangeIntegerSet& cities_having_2_edges,
+                        mpi::LimitedRangeIntegerSet& cities_having_just_1_edge,
+                        mpi::ObjectPool<std::vector<size_t>>& any_size_vector_pool)
+{
+    bool starts_with_B = finding_path.size() % 2 == 0;
+    size_t start_index = finding_path.size() - 1;
+    auto AB_cycle_ptr = any_size_vector_pool.acquire_unique();
+    auto& AB_cycle = *AB_cycle_ptr;
+    AB_cycle.clear();
+    AB_cycle.reserve(start_index - end_index);
+    
+    size_t last = 0;
+    if (starts_with_B) {
+        last = finding_path[start_index];
+        if (cities_having_2_edges.contains(last)) {
+            cities_having_2_edges.erase(last);
+            cities_having_just_1_edge.insert(last);
+        } else if (cities_having_just_1_edge.contains(last)) {
+            cities_having_just_1_edge.erase(last);
+        }
+        start_index -= 1;
+    }
+    
+    for (size_t i = start_index; i > end_index; i -= 1) {
+        size_t current = finding_path[i];
+        AB_cycle.push_back(current);
+        if (cities_having_2_edges.contains(current)) {
+            cities_having_2_edges.erase(current);
+            cities_having_just_1_edge.insert(current);
+        } else if (cities_having_just_1_edge.contains(current)) {
+            cities_having_just_1_edge.erase(current);
+        }
+    }
+    
+    if (starts_with_B) {
+        AB_cycle.push_back(last);
+    }
+    
+    finding_path.resize(end_index + 1);
+    
+    return AB_cycle_ptr;
+}
+
+/**
+ * @brief ABサイクルを見つける
+ * @param needs 必要なABサイクルの数
+ * @param parent1 親個体1
+ * @param parent2 親個体2
+ * @param rng 乱数生成器
+ * @param any_size_vector_pool 汎用サイズのベクタープール
+ * @param vector_of_tsp_size_pool TSPの都市数と同じサイズのベクタープール
+ * @param doubly_linked_list_pool TSPの都市数と同じサイズの双方向リストプール
+ * @param LRIS_pool 0 ~ (TSPの都市数 - 1)の整数を管理する集合のプール
+ * @return ABサイクルのポインタのベクター
+ * @tparam Individual 親個体の型
+ * @pre Individualは、 双方向リストとして扱える必要がある。
+ * @pre Individualは、`size()`メソッドを持ち、都市数を返す必要がある。
+ */
+template <typename Individual>
+    requires requires (const Individual& ind, size_t a) {
+        { ind[a] } -> std::convertible_to<const std::array<size_t, 2>&>;
+        { ind.size() } -> std::convertible_to<size_t>;
+    }
+std::vector<mpi::ObjectPool<std::vector<size_t>>::pooled_unique_ptr> find_AB_cycles(size_t needs,
+            const Individual& parent1,
+            const Individual& parent2,
+            std::mt19937& rng,
+            mpi::ObjectPool<std::vector<size_t>>& any_size_vector_pool,
+            mpi::ObjectPool<std::vector<size_t>>& vector_of_tsp_size_pool,
+            mpi::ObjectPool<std::vector<std::array<size_t, 2>>>& doubly_linked_list_pool,
+            mpi::ObjectPool<mpi::LimitedRangeIntegerSet>& LRIS_pool)
+{
+    using namespace std;
+    const size_t city_count = parent1.size();
+    // mpi::LimitedRangeIntegerSet cities_having_2_edges(city_count - 1, mpi::LimitedRangeIntegerSet::InitSet::Universal);
+    // mpi::LimitedRangeIntegerSet cities_having_just_1_edge(city_count - 1, mpi::LimitedRangeIntegerSet::InitSet::Empty);
+    auto cities_having_2_edges_ptr = LRIS_pool.acquire_unique();
+    auto cities_having_just_1_edge_ptr = LRIS_pool.acquire_unique();
+    auto& cities_having_2_edges = *cities_having_2_edges_ptr;
+    auto& cities_having_just_1_edge = *cities_having_just_1_edge_ptr;
+    cities_having_2_edges.reset(mpi::LimitedRangeIntegerSet::InitSet::Universal);
+    cities_having_just_1_edge.reset(mpi::LimitedRangeIntegerSet::InitSet::Empty);
+    struct connections {
+        std::array<size_t, 2>& edge_pair;
+        constexpr size_t least_recently_used_edge() const {
+            return edge_pair[0];
+        }
+        
+        constexpr size_t least_recently_used_edge_and_update() {
+            std::swap(edge_pair[0], edge_pair[1]);
+            return edge_pair[1];
+        }
+
+        constexpr size_t get_and_update(size_t index) {
+            if (index == 0) {
+                std::swap(edge_pair[0], edge_pair[1]);
+            }
+            
+            return edge_pair[1];
+        }
+        
+        constexpr void update(size_t prev) {
+            if (prev == edge_pair[0]) {
+                std::swap(edge_pair[0], edge_pair[1]);
+            }
+        }
+    };
+
+    auto parent1_copy_ptr = doubly_linked_list_pool.acquire_unique();
+    auto parent2_copy_ptr = doubly_linked_list_pool.acquire_unique();
+    auto& parent1_copy = *parent1_copy_ptr;
+    auto& parent2_copy = *parent2_copy_ptr;
+    for (size_t i = 0; i < city_count; ++i) {
+        parent1_copy[i] = parent1[i];
+        parent2_copy[i] = parent2[i];
+    }
+    
+    struct parent {
+        std::vector<std::array<size_t, 2>>& parent;
+        constexpr connections operator[](size_t index) {
+            return connections{parent[index]};
+        }
+    };
+    
+    struct {
+        parent parent1;
+        parent parent2;
+        constexpr parent operator[](size_t index) {
+            return index == 0 ? parent1 : parent2;
+        }
+    } parents = {{parent1_copy}, {parent2_copy}};
+
+    vector<mpi::ObjectPool<std::vector<size_t>>::pooled_unique_ptr> AB_cycles;
+
+    uniform_int_distribution<size_t> dist_01(0, 1);
+    
+    // vector<size_t> visited;
+    // vector<size_t> first_visited(city_count, 0);
+    auto visited_ptr = any_size_vector_pool.acquire_unique();
+    auto first_visited_ptr = vector_of_tsp_size_pool.acquire_unique();
+    auto& visited = *visited_ptr;
+    auto& first_visited = *first_visited_ptr;
+    visited.clear();
+    first_visited.assign(city_count, 0);
+    size_t current_city = numeric_limits<size_t>::max();
+
+    while (cities_having_2_edges.size() > 0) {
+
+        if (!(cities_having_2_edges.contains(current_city) ||
+            cities_having_just_1_edge.contains(current_city))) { // 現在の都市にエッジが存在しない場合
+
+            // 2つエッジを持つ都市からランダムに選択
+            size_t rand_to_select_city = uniform_int_distribution<size_t>(0, cities_having_2_edges.size() - 1)(rng);
+            current_city = *(cities_having_2_edges.begin() + rand_to_select_city);
+            
+            visited.clear();
+            visited.push_back(current_city);
+            first_visited[current_city] = 0;
+        }
+        
+        while (true) {  // ABサイクルを一つ見つけるまでループ
+            size_t prev_city = current_city;
+            size_t prev_first_visited = first_visited[prev_city];
+            size_t prev_edge_count = cities_having_2_edges.contains(current_city) ? 2 : 1;
+            size_t next_edge_parent = visited.size() % 2;
+            
+            if (prev_edge_count == 1) { // エッジが一つなら
+                // 単純に最近最も使用されていないエッジを選択
+                // もうほかの枝は存在しないので LRU は更新しない
+                current_city = parents[next_edge_parent][prev_city].least_recently_used_edge();
+            } else {
+                if (prev_first_visited != visited.size() - 1) {
+                    // 2回訪れた都市なら、残り一つのエッジを選択
+                    // prev_first_visitedが直前じゃなければ、2回訪れたことが分かる
+                    current_city = parents[next_edge_parent][prev_city].least_recently_used_edge_and_update();
+                } else { // そうじゃないなら
+                    current_city = parents[next_edge_parent][prev_city].get_and_update(dist_01(rng));
+                }
+            }
+            
+            visited.push_back(current_city);
+
+            size_t current_edge_count = cities_having_2_edges.contains(current_city) ? 2 : 1;
+            if (current_edge_count == 1) {
+                // エッジの数が1なら、探索途中か、スタートにたどり着いて一周したか
+                if (current_city == visited.front()) {
+                    // ABサイクル構成処理
+                    auto cycle = create_AB_cycle(visited, 0, cities_having_2_edges, cities_having_just_1_edge, any_size_vector_pool);
+                    if (cycle->size() > 2) {
+                        AB_cycles.emplace_back(std::move(cycle));
+                    }
+                    break;
+                } else {
+                    continue; // 探索途中なら、次の都市へ
+                }
+            } else {
+                // エッジの数が2なら、探索途中か、スタートにたどり着いて一周したか、途中で交差してABサイクルを構成するか
+                // 次の都市のLRUを更新
+                parents[next_edge_parent][current_city].update(prev_city);
+                if (current_city == visited.front()) {
+                    if ((visited.size() + 1) % 2 == 0) { // Bで出てAで帰ってきた
+                        // ABサイクル構成処理
+                        auto cycle = create_AB_cycle(visited, 0, cities_having_2_edges, cities_having_just_1_edge, any_size_vector_pool);
+                        if (cycle->size() > 2) {
+                            AB_cycles.emplace_back(std::move(cycle));
+                        }
+                        break;
+                    } else { // Bで出てBで帰ってきた
+                        continue;
+                    }
+                } else if (first_visited[current_city] != 0 && (visited.size() - first_visited[current_city] + 1) % 2 == 0) {
+                    // 交差している　かつ　ABサイクルを構成するなら
+                    auto cycle = create_AB_cycle(visited, first_visited[current_city], cities_having_2_edges, cities_having_just_1_edge, any_size_vector_pool);
+                    if (cycle->size() > 2) {
+                        AB_cycles.emplace_back(std::move(cycle));
+                    }
+                    break;
+                } else if (first_visited[current_city] != 0) {
+                    continue;
+                } else {
+                    first_visited[current_city] = visited.size() - 1;
+                    continue;
+                }
+            }
+        }
+        
+        if (AB_cycles.size() >= needs) {
+            return AB_cycles; // 必要な数のABサイクルが見つかった
+        }
+    }
+
+    while (cities_having_just_1_edge.size() > 0) {
+        size_t start_city = *(cities_having_just_1_edge.begin());
+
+        // 1つのエッジを持つ都市からABサイクルを構成する
+        visited.clear();
+        visited.push_back(start_city);
+
+        size_t current_city = start_city;
+        while (true) {
+            size_t next_edge_parent = visited.size() % 2;
+            size_t next_city = parents[next_edge_parent][current_city].least_recently_used_edge();
+            visited.push_back(next_city);
+            if (next_city == start_city) 
+                break;
+            current_city = next_city;
+        }
+        
+        auto cycle = create_AB_cycle(visited, 0, cities_having_2_edges, cities_having_just_1_edge, any_size_vector_pool);
+        if (cycle->size() > 2) {
+            AB_cycles.emplace_back(std::move(cycle));
+        }
+        
+        if (AB_cycles.size() >= needs) {
+            return AB_cycles; // 必要な数のABサイクルが見つかった
+        }
+    }
+    
+    return AB_cycles;
+}
+}
