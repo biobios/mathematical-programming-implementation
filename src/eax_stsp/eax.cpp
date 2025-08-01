df0f4bd41eeea2ae529725d2a2afe0d855291591
BioBIOS <biobios1024@gmail.com>
2025-08-01 16:35:37 +0900

    feat: eaxlibのfind_AB_cycles関数を使用するように変更し、影響する部分を修正

--- src/eax_stsp/eax.cpp
+++ src/eax_stsp/eax.cpp
@@ -14,7 +14,11 @@
 #include <iostream>
 #include <map>
 
+#include "ab_cycle_finder.hpp"
+#include "limited_range_integer_set.hpp"
+
 namespace {
+using AB_cycle_ptr = mpi::ObjectPool<std::vector<size_t>>::pooled_unique_ptr;
 struct edge {
     size_t v1;
     size_t v2;
@@ -44,7 +48,7 @@ std::vector<double> times2(5, 0.0);
 void step_1(const std::vector<size_t>& parent1, const std::vector<size_t>& parent2,
              std::vector<std::array<size_t, 2>>& adjacency_list_parent1,
              std::vector<std::array<size_t, 2>>& adjacency_list_parent2,
-             std::vector<std::vector<size_t>>& prototype_child,
+             std::vector<std::array<size_t, 2>>& prototype_child,
              size_t n) {
     auto start_time = std::chrono::high_resolution_clock::now();
     for (size_t i = 0; i < n; ++i) {
@@ -183,49 +187,41 @@ void step_2(std::vector<std::array<size_t, 2>> adjacency_list_parent1,
     
 }
 
-void step_3_and_4(std::vector<std::vector<size_t>>& child_adjacency_list,
-                  const std::vector<std::vector<std::pair<edge, bool>>>& AB_cycles,
+void step_3_and_4(std::vector<std::array<size_t, 2>>& child_adjacency_list,
+                  const std::vector<AB_cycle_ptr>& AB_cycles,
                   std::mt19937& rng)
 {
     auto start_time = std::chrono::high_resolution_clock::now();
     using namespace std;
     uniform_int_distribution<size_t> dist_01(0, 1);
-    for (const auto& AB_cycle : AB_cycles) {
+    for (const auto& AB_cycle_ptr : AB_cycles) {
         if (dist_01(rng) == 0)
             continue; // このABサイクルは使用しない
         
-        if (AB_cycle.front().second) {
-            for (size_t i = 0; i < AB_cycle.size(); i += 2) {
-                const auto& [e, from_parent1] = AB_cycle[i];
-                const auto& [v1, v2] = e;
-                auto it = find(child_adjacency_list[v1].begin(), child_adjacency_list[v1].end(), v2);
-                child_adjacency_list[v1].erase(it);
-                it = find(child_adjacency_list[v2].begin(), child_adjacency_list[v2].end(), v1);
-                child_adjacency_list[v2].erase(it);
+        const auto& AB_cycle = *AB_cycle_ptr;
+        
+        auto swap = [&child_adjacency_list](size_t b1, size_t ba, size_t ab, size_t b2) {
+            if (child_adjacency_list[ba][0] == ab) {
+                child_adjacency_list[ba][0] = b1;
+            } else {
+                child_adjacency_list[ba][1] = b1;
             }
-            
-            for (size_t i = 1; i < AB_cycle.size(); i += 2) {
-                const auto& [e, from_parent1] = AB_cycle[i];
-                const auto& [v1, v2] = e;
-                child_adjacency_list[v1].push_back(v2);
-                child_adjacency_list[v2].push_back(v1);
+            if (child_adjacency_list[ab][0] == ba) {
+                child_adjacency_list[ab][0] = b2;
+            } else {
+                child_adjacency_list[ab][1] = b2;
             }
-        } else {
-            for (size_t i = 1; i < AB_cycle.size(); i += 2) {
-                const auto& [e, from_parent1] = AB_cycle[i];
-                const auto& [v1, v2] = e;
-                auto it = find(child_adjacency_list[v1].begin(), child_adjacency_list[v1].end(), v2);
-                child_adjacency_list[v1].erase(it);
-                it = find(child_adjacency_list[v2].begin(), child_adjacency_list[v2].end(), v1);
-                child_adjacency_list[v2].erase(it);
-            }
-
-            for (size_t i = 0; i < AB_cycle.size(); i += 2) {
-                const auto& [e, from_parent1] = AB_cycle[i];
-                const auto& [v1, v2] = e;
-                child_adjacency_list[v1].push_back(v2);
-                child_adjacency_list[v2].push_back(v1);
-            }
+        };
+        for (size_t i = 2; i < AB_cycle.size() - 2; i += 2) {
+            swap(AB_cycle[i - 1], AB_cycle[i], AB_cycle[i + 1], AB_cycle[i + 2]);
+        }
+        // i = 0
+        {
+            swap(AB_cycle[AB_cycle.size() - 1], AB_cycle[0], AB_cycle[1], AB_cycle[2]);
+        }
+        // i = cycle.size() - 2
+        {
+            swap(AB_cycle[AB_cycle.size() - 3], AB_cycle[AB_cycle.size() - 2], AB_cycle[AB_cycle.size() - 1], AB_cycle[0]);
         }
     }
     
@@ -233,7 +229,7 @@ void step_3_and_4(std::vector<std::vector<size_t>>& child_adjacency_list,
     times[2] += std::chrono::duration<double>(end_time - start_time).count();
 }
 
-void step_5_and_6(std::vector<std::vector<size_t>>& child_adjacency_list,
+void step_5_and_6(std::vector<std::array<size_t, 2>>& child_adjacency_list,
             const std::vector<std::vector<int64_t>>& adjacency_matrix,
             std::vector<size_t>& child_path,
             size_t n,
@@ -244,38 +240,28 @@ void step_5_and_6(std::vector<std::vector<size_t>>& child_adjacency_list,
     using distance_type = std::remove_cvref_t<decltype(adjacency_matrix)>::value_type::value_type;
     vector<size_t> belongs_to_cycle(n, n); // 各都市がどの部分巡回路に属するかを記録
     vector<vector<size_t>> cycles_cities; // 各部分巡回路に属する都市のリスト
-    vector<pair<size_t, size_t>> double_linked_list(n, {n, n});
-    size_t remaining_edges = n;
+    mpi::LimitedRangeIntegerSet unvisited_cities(n - 1, mpi::LimitedRangeIntegerSet::InitSet::Universal);
     size_t cycle_index = 0;
-    
-    while (remaining_edges > 0) {
-        vector<size_t> cycle_cities;
-        size_t current_city = 0;
-        
-        for (size_t i = 0; i < n; ++i) {
-            if (!child_adjacency_list[i].empty()) {
-                current_city = i;
-                break;
-            }
-        }
-        size_t prev_city = 0;
-        while (!child_adjacency_list[current_city].empty()) {
-            auto next_city = child_adjacency_list[current_city].back();
-            // リストから現在のエッジを削除
-            child_adjacency_list[current_city].pop_back();
-            // 逆向きも削除
-            auto it = find(child_adjacency_list[next_city].begin(), child_adjacency_list[next_city].end(), current_city);
-            child_adjacency_list[next_city].erase(it);
 
-            belongs_to_cycle[current_city] = cycle_index;
+    while (unvisited_cities.size() > 0) {
+        vector<size_t> cycle_cities;
+        size_t start_city = *unvisited_cities.begin();
+        size_t current_city = start_city;
+        size_t prev_city = start_city;
+        do {
+            // 現在の都市を部分巡回路に追加
             cycle_cities.push_back(current_city);
-            double_linked_list[current_city] = {prev_city, next_city};
+            belongs_to_cycle[current_city] = cycle_index;
+            // 現在の都市を unvisited_cities から削除
+            unvisited_cities.erase(current_city);
+            auto next_city = child_adjacency_list[current_city][0];
+            if (next_city == prev_city) {
+                next_city = child_adjacency_list[current_city][1];
+            }
+            
             prev_city = current_city;
             current_city = next_city;
-            remaining_edges -= 1;
-        }
-        
-        double_linked_list[current_city].first = prev_city;
+        } while (current_city != start_city);
 
         cycles_cities.emplace_back(move(cycle_cities));
         cycle_index += 1;
@@ -328,17 +314,17 @@ void step_5_and_6(std::vector<std::vector<size_t>>& child_adjacency_list,
                         }
 
                         // 8通りの接続のコストを計算
-                        pair<size_t, size_t> connected_city_pair_v1 = double_linked_list[v1_index];
-                        pair<size_t, size_t> connected_city_pair_v2 = double_linked_list[v2_index];
+                        array<size_t, 2> connected_city_pair_v1 = child_adjacency_list[v1_index];
+                        array<size_t, 2> connected_city_pair_v2 = child_adjacency_list[v2_index];
                         
                         array<edge, 2> edges1 = {
-                            edge(v1_index, connected_city_pair_v1.first),
-                            edge(v1_index, connected_city_pair_v1.second)
+                            edge(v1_index, connected_city_pair_v1[0]),
+                            edge(v1_index, connected_city_pair_v1[1])
                         };
                         
                         array<edge, 2> edges2 = {
-                            edge(v2_index, connected_city_pair_v2.first),
-                            edge(v2_index, connected_city_pair_v2.second)
+                            edge(v2_index, connected_city_pair_v2[0]),
+                            edge(v2_index, connected_city_pair_v2[1])
                         };
                         
                         for (const auto& edge1 : edges1) {
@@ -368,59 +354,59 @@ void step_5_and_6(std::vector<std::vector<size_t>>& child_adjacency_list,
         // 見つかった辺を使って部分巡回路を接続
         if (forward_connection) {
     //         e1.v2 -> e2.v2;
-            if (double_linked_list[e1.v1].first == e1.v2) {
-                double_linked_list[e1.v1].first = e2.v2;
+            if (child_adjacency_list[e1.v1][0] == e1.v2) {
+                child_adjacency_list[e1.v1][0] = e2.v2;
             } else {
-                double_linked_list[e1.v1].second = e2.v2;
+                child_adjacency_list[e1.v1][1] = e2.v2;
             }
 
     //         e1.v1 -> e2.v1;
-            if (double_linked_list[e1.v2].first == e1.v1) {
-                double_linked_list[e1.v2].first = e2.v1;
+            if (child_adjacency_list[e1.v2][0] == e1.v1) {
+                child_adjacency_list[e1.v2][0] = e2.v1;
             } else {
-                double_linked_list[e1.v2].second = e2.v1;
+                child_adjacency_list[e1.v2][1] = e2.v1;
             }
             
     //         e2.v2 -> e1.v2;
-            if (double_linked_list[e2.v1].first == e2.v2) {
-                double_linked_list[e2.v1].first = e1.v2;
+            if (child_adjacency_list[e2.v1][0] == e2.v2) {
+                child_adjacency_list[e2.v1][0] = e1.v2;
             } else {
-                double_linked_list[e2.v1].second = e1.v2;
+                child_adjacency_list[e2.v1][1] = e1.v2;
             }
             
     //         e2.v1 -> e1.v1;
-            if (double_linked_list[e2.v2].first == e2.v1) {
-                double_linked_list[e2.v2].first = e1.v1;
+            if (child_adjacency_list[e2.v2][0] == e2.v1) {
+                child_adjacency_list[e2.v2][0] = e1.v1;
             } else {
-                double_linked_list[e2.v2].second = e1.v1;
+                child_adjacency_list[e2.v2][1] = e1.v1;
             }
         } else {
     //         e1.v2 -> e2.v1;
-            if (double_linked_list[e1.v1].first == e1.v2) {
-                double_linked_list[e1.v1].first = e2.v1;
+            if (child_adjacency_list[e1.v1][0] == e1.v2) {
+                child_adjacency_list[e1.v1][0] = e2.v1;
             } else {
-                double_linked_list[e1.v1].second = e2.v1;
+                child_adjacency_list[e1.v1][1] = e2.v1;
             }
 
     //         e1.v1 -> e2.v2;
-            if (double_linked_list[e1.v2].first == e1.v1) {
-                double_linked_list[e1.v2].first = e2.v2;
+            if (child_adjacency_list[e1.v2][0] == e1.v1) {
+                child_adjacency_list[e1.v2][0] = e2.v2;
             } else {
-                double_linked_list[e1.v2].second = e2.v2;
+                child_adjacency_list[e1.v2][1] = e2.v2;
             }
             
     //         e2.v2 -> e1.v1;
-            if (double_linked_list[e2.v1].first == e2.v2) {
-                double_linked_list[e2.v1].first = e1.v1;
+            if (child_adjacency_list[e2.v1][0] == e2.v2) {
+                child_adjacency_list[e2.v1][0] = e1.v1;
             } else {
-                double_linked_list[e2.v1].second = e1.v1;
+                child_adjacency_list[e2.v1][1] = e1.v1;
             }
             
     //         e2.v1 -> e1.v2;
-            if (double_linked_list[e2.v2].first == e2.v1) {
-                double_linked_list[e2.v2].first = e1.v2;
+            if (child_adjacency_list[e2.v2][0] == e2.v1) {
+                child_adjacency_list[e2.v2][0] = e1.v2;
             } else {
-                double_linked_list[e2.v2].second = e1.v2;
+                child_adjacency_list[e2.v2][1] = e1.v2;
             }
         }
         
@@ -452,9 +438,9 @@ void step_5_and_6(std::vector<std::vector<size_t>>& child_adjacency_list,
     size_t current_city = 0;
     for (size_t i = 0; i < n; ++i) {
         child_path[i] = current_city;
-        auto next_city = double_linked_list[current_city].first;
+        auto next_city = child_adjacency_list[current_city][0];
         if (next_city == prev_city) {
-            next_city = double_linked_list[current_city].second;
+            next_city = child_adjacency_list[current_city][1];
         }
         prev_city = current_city;
         current_city = next_city;
@@ -486,13 +472,10 @@ std::vector<std::vector<size_t>> edge_assembly_crossover(const std::vector<size_
     vector<size_t> dummy(n);
     vector<array<size_t, 2>> adjacency_list_parent1(n);
     vector<array<size_t, 2>> adjacency_list_parent2(n);
-    vector<vector<size_t>> prototype_child(n, vector<size_t>(2));
+    vector<array<size_t, 2>> prototype_child(n);
     step_1(parent1, parent2, adjacency_list_parent1, adjacency_list_parent2, prototype_child, n);
     
-    vector<vector<edge_with_parent>> AB_cycles;
-    
-    
-    step_2(adjacency_list_parent1, adjacency_list_parent2, AB_cycles, rng, n);
+    auto AB_cycles = find_AB_cycles(std::numeric_limits<size_t>::max(), adjacency_list_parent1, adjacency_list_parent2, rng, env.object_pools.any_size_vector_pool, env.object_pools.vector_of_tsp_size_pool, env.object_pools.doubly_linked_list_pool, env.object_pools.LRIS_pool);
     
     vector<vector<size_t>> children(children_size);
     for (size_t child_index = 0; child_index < children_size; ++child_index) {
@@ -509,7 +492,7 @@ std::vector<std::vector<size_t>> edge_assembly_crossover(const std::vector<size_
         }
         
         // 緩和個体を作成
-        vector<vector<size_t>> child_adjacency_list = prototype_child;
+        vector<array<size_t, 2>> child_adjacency_list = prototype_child;
         
         step_3_and_4(child_adjacency_list, AB_cycles, rng);
 
