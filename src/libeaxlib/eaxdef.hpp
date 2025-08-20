9117e46abbdbb16cf56837af4e266bd89eaa1246
BioBIOS <biobios1024@gmail.com>
2025-08-20 20:39:22 +0900

    feat: eaxdef.hppファイルを追加し、隣接行列やABサイクルの型を定義

---
+++ src/libeaxlib/eaxdef.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <cstdint>
+#include <vector>
+#include <array>
+
+namespace eax {
+using adjacency_matrix_t = std::vector<std::vector<int64_t>>;
+using NN_list_t = std::vector<std::vector<size_t>>;
+using ab_cycle_t = std::vector<size_t>;
+using doubly_linked_list_t = std::vector<std::array<size_t, 2>>;
+
+template <typename T>
+concept doubly_linked_list_like = requires(T t) {
+    { t[0][0] } -> std::convertible_to<size_t>;
+    { t[0][1] } -> std::convertible_to<size_t>;
+    { t.size() } -> std::convertible_to<size_t>;
+};
+}
