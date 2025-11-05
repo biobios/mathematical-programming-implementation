31c43ea5d9cd41f58ae220a983a704e1159c9a21
BioBIOS <biobios1024@gmail.com>
2025-11-05 10:48:32 +0900

    feat: EAX_N_ABを復元抽出に変更

--- src/libeaxlib/eax_n_ab.hpp
+++ src/libeaxlib/eax_n_ab.hpp
@@ -18,47 +18,20 @@ public:
     N_AB_e_set_assembler(size_t ab_cycle_count, size_t N_parameter, mpi::ObjectPool<std::vector<size_t>>&& any_size_vector_pool, std::mt19937& rng)
         : ab_cycle_count(ab_cycle_count),
             N_parameter(N_parameter),
-            any_size_vector_pool(std::move(any_size_vector_pool)),
-            indices_of_remaining_AB_cycles(this->any_size_vector_pool.acquire_unique()) {
-
-        auto& indices = *indices_of_remaining_AB_cycles;
-        indices.clear();
-        indices.resize(ab_cycle_count);
-
-        std::iota(indices.begin(), indices.end(), 0);
-        std::shuffle(indices.begin(), indices.end(), rng);
-    }
+            any_size_vector_pool(std::move(any_size_vector_pool)) {}
     
     bool has_next() const {
-        if (N_parameter == 1 || ab_cycle_count <= N_parameter) {
-            // シャッフルしなおしても必ず同じ組み合わせになる場合、
-            // 残りのサイクルを使いきったら終了
-            return indices_of_remaining_AB_cycles->size() > 0;
-        }
-        
-        // シャッフルしなおすことで別の組み合わせが生成可能な場合、常に真
         return true;
     }
     
     mpi::pooled_unique_ptr<std::vector<size_t>> next(std::mt19937& rng) {
-        auto& indices = *indices_of_remaining_AB_cycles;
-        
-        if (indices.size() < N_parameter && ab_cycle_count > N_parameter && N_parameter > 1) {
-            // 残りのサイクルがN_parameter未満で、かつ、シャッフルしなおすことで別の組み合わせを生成可能な場合
-            indices.resize(ab_cycle_count);
-            std::iota(indices.begin(), indices.end(), 0);
-            std::shuffle(indices.begin(), indices.end(), rng);
-        }
-
         auto selected_AB_cycles_indices_ptr = any_size_vector_pool.acquire_unique();
         auto& selected_AB_cycles_indices = *selected_AB_cycles_indices_ptr;
-        selected_AB_cycles_indices.clear();
-
-        for (size_t i = 0; i < N_parameter && !indices.empty(); ++i) {
-            size_t index = indices.back();
-            indices.pop_back();
-            
-            selected_AB_cycles_indices.push_back(index);
+        selected_AB_cycles_indices.resize(ab_cycle_count);
+        std::iota(selected_AB_cycles_indices.begin(), selected_AB_cycles_indices.end(), 0);
+        std::shuffle(selected_AB_cycles_indices.begin(), selected_AB_cycles_indices.end(), rng);
+        if (selected_AB_cycles_indices.size() > N_parameter) {
+            selected_AB_cycles_indices.resize(N_parameter);
         }
         return selected_AB_cycles_indices_ptr;
     }
@@ -66,8 +39,6 @@ private:
     size_t ab_cycle_count;
     size_t N_parameter;
     mpi::ObjectPool<std::vector<size_t>> any_size_vector_pool;
-
-    mpi::pooled_unique_ptr<std::vector<size_t>> indices_of_remaining_AB_cycles;
 };
 
 class N_AB_e_set_assembler_builder {
