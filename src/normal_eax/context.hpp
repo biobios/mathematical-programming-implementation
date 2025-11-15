e4dedbefbbddba2bfc01805f6cdd6096cce2380b
BioBIOS <biobios1024@gmail.com>
2025-11-15 11:56:29 +0900

    feat: Contextにエントロピーを追加し、初期エッジカウント設定時にエントロピーを計算

--- src/normal_eax/context.hpp
+++ src/normal_eax/context.hpp
@@ -63,6 +63,8 @@ namespace eax {
         std::chrono::system_clock::time_point start_time;
         // 経過時間
         double elapsed_time = 0.0;
+        // エントロピー
+        double entropy = 0.0;
 
         void set_initial_edge_counts(const std::vector<Individual>& init_pop) {
             pop_edge_counts.assign(env.tsp.city_count, std::vector<size_t>(env.tsp.city_count, 0));
@@ -73,6 +75,16 @@ namespace eax {
                     size_t v2 = individual[i][1];
                     pop_edge_counts[i][v1] += 1;
                     pop_edge_counts[i][v2] += 1;
+                }
+            }
+
+            entropy = 0.0;
+            for (auto& row : pop_edge_counts) {
+                for (auto& count : row) {
+                    double p = static_cast<double>(count) / static_cast<double>(env.population_size);
+                    if (p > 0.0) {
+                        entropy += -p * std::log2(p);
+                    }
                 }
             }
         };
