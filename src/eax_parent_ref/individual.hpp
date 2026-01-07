4f4a12d4038113024a300c3d2f77a42cbfe4e19b
BioBIOS <biobios1024@gmail.com>
2026-01-08 03:24:26 +0900

    feat: ParentReferenceMergerを用いたプログラムを実装

---
+++ src/eax_parent_ref/individual.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <vector>
+#include <array>
+#include <iostream>
+#include <cmath>
+#include <chrono>
+
+#include "crossover_delta.hpp"
+
+namespace eax {
+    class Individual;
+
+    class Individual {
+    public:
+        Individual() = default;
+        Individual(const std::vector<size_t>& path, const std::vector<std::vector<int64_t>>& adjacency_matrix);
+        constexpr std::array<size_t, 2>& operator[](size_t index) {
+            return doubly_linked_list[index];
+        }
+
+        constexpr std::array<size_t, 2> const& operator[](size_t index) const {
+            return doubly_linked_list[index];
+        }
+
+        size_t size() const;
+        size_t get_distance() const {
+            return distance;
+        }
+        std::vector<size_t> to_path() const;
+        Individual& operator=(const CrossoverDelta& child) {
+            prev_diff = child;
+            return *this;
+        }
+        
+        CrossoverDelta update(const std::vector<std::vector<int64_t>>& adjacency_matrix) {
+            CrossoverDelta child = std::move(prev_diff);
+            prev_diff = CrossoverDelta();
+            child.apply_to(*this);
+            distance += child.get_delta_distance(adjacency_matrix);
+            return child;
+        }
+        
+        void serialize(std::ostream& os) const;
+        static Individual deserialize(std::istream& is);
+    private:
+        std::vector<std::array<size_t, 2>> doubly_linked_list;
+        CrossoverDelta prev_diff;
+        int64_t distance = 0;
+    };
+}
